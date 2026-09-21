e9f807de0f5e687698b809d70f4f29ec713c036e
moez chroufa <chroufa.moezkhan@gmail.com>
2026-09-21 21:42:05 +0100

    child process creation

---
+++ cmd.js
@@ -0,0 +1,20 @@
+const { spawn } = require("child_process");
+
+const spawnOptions = {
+  shell: true,
+};
+
+function cmd(prog, args) {
+  console.log("CMD:", prog, args.flat(), spawnOptions);
+  const p = spawn(prog, args.flat(), spawnOptions);
+  p.stdout.on("data", (data) => process.stdout.write(data));
+  p.stderr.on("data", (data) => process.stderr.write(data));
+  p.on("close", (code) => {
+    if (code !== 0) {
+      console.error(prog, args, `exited with ${code}`);
+    }
+  });
+  return p;
+}
+// testing
+cmd("tsx", ["normError.a.ts"]);
